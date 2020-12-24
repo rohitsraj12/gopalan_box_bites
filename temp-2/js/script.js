24f55801c5c1ef573a7f868f33103501d0bd5eff
Rohit <rohitsraj12@gmail.com>
2020-12-24 16:43:16 +0530

    Create script.js

---
+++ temp-2/js/script.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+    var owl = $('.banner__offer .owl-carousel');
+    owl.owlCarousel({
+    loop: true,
+    items: 3,
+    margin: 0,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:false
+        },
+        600:{
+            items:3,
+            nav:false,
+            stagePadding: 100
+        },
+        1000:{
+            items:3,
+            nav:false,
+            margin: 5,
+            center: true,
+            stagePadding: 100
+        }
+    }
+    });
+
+    // Go to the next item
+    $('.customNextBtn').click(function() {
+        owl.trigger('next.owl.carousel');
+    })
+
+    // Go to the previous item
+    $('.customPrevBtn').click(function() {
+        // With optional speed parameter
+        // Parameters has to be in square bracket '[]'
+        owl.trigger('prev.owl.carousel', [300]);
+    })
+})
